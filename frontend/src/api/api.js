a9d6b11fe95329f512f8149e46b2d7b4c04570a7
Saurabh Upadhyay <upadhyaysaurabh037@gmail.com>
2026-04-18 18:44:15 +0530

    fix vercel routing

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://dsa-tracker-opjw.onrender.com/api",
 });
 
 export default API;
